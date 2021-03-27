dfd5f89886b63b02b555c7cd77c0103287878313
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 22:42:45 -0400

    initial

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+        title:'Minigame Core Docs',
+        description:'Documentation for Minigame Core',
+        themeConfig: {
+                sidebar:"auto",
+                search:false,
+        }
+}
+
